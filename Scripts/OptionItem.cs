539244408b67796cc378ac33c96f5e2fd93e0d1b
Miguel Moronta <miguel@moronta.com.do>
2019-03-16 11:41:35 -0400

    El menú principal ya está listo en esta versión, y se han agregado otros recursos para practicar con la creación de la segunda escena (navegación).

---
+++ Scripts/OptionItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionItem : MonoBehaviour
+{
+    const float HOVERFACTOR = 1.2f;
+    SoundsManager SoundsManager;
+    private void Awake()
+    {
+        SoundsManager = GameObject.Find("SoundsManager").GetComponent<SoundsManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        SoundsManager.SendMessage("PlayHoverSound");
+        gameObject.transform.localScale *= HOVERFACTOR;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.transform.localScale /= HOVERFACTOR;
+    }
+
+    private void OnMouseUp()
+    {
+        switch(gameObject.name)
+        {
+            case "PlayOption":
+                SoundsManager.SendMessage("PlayClickSound");
+                break;
+            case "PrefsOption":
+                SoundsManager.SendMessage("PlayClickSound");
+                break;
+            case "CreditsOption":
+                SoundsManager.SendMessage("PlayClickSound");
+                break;
+            case "ExitOption":
+                SoundsManager.SendMessage("PlayClickSound");
+                Application.Quit();
+                break;
+        }
+    }
+}
---
+++ Scripts/SoundsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsManager : MonoBehaviour
+{
+    public AudioSource ClickSound;
+    public AudioSource HoverSound;
+    
+    public void PlayClickSound()
+    {
+        ClickSound.Play();
+    }
+
+    public void PlayHoverSound()
+    {
+        HoverSound.Play();
+    }
+}
