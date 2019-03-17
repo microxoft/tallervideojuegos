4c2af0d6ce70df724f10b737b2bc94d142e2140c
Miguel Moronta <miguel@moronta.com.do>
2019-03-17 14:03:16 -0400

    Versión final del taller de desarrollo de videojuegos.

---
+++ Pirata RPG/Assets/Scripts/MapManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    XmlDocument level1;
+    public GameObject DirtA, FlowerK, FlowerL, FlowerM, GrassE, PathJ, PathK, PathL, PathM, PathN, PathO, PathP, PathQ, PathR, PathS, PathT, PathU, PathV, PathW, PathX, PathY, PathZ, RockN, RockO, RockP, RockQ, TreeA, TreeB, TreeC, TreeD, TreeE, WallR, WallS, WallT, WallU, WaterW, WaterX, WaterY, WaterZ;
+    public GameObject PiraGuchi, Enemy3F, Enemy3M;
+    Dictionary<char, GameObject> prefabs;
+    GameObject newCell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabs = new Dictionary<char, GameObject> {
+            { 'A', TreeA },
+            { 'B', TreeB },
+            { 'C', TreeC },
+            { 'D', TreeD },
+            { 'E', TreeE },
+            { 'j', PathJ },
+            { 'k', PathK },
+            { 'l', PathL },
+            { 'm', PathM },
+            { 'n', PathN },
+            { 'o', PathO },
+            { 'p', PathP },
+            { 'q', PathQ },
+            { 'r', PathR },
+            { 's', PathS },
+            { 't', PathT },
+            { 'u', PathU },
+            { 'v', PathV },
+            { 'w', PathW },
+            { 'x', PathX },
+            { 'y', PathY },
+            { 'z', PathZ },
+            { 'K', FlowerK },
+            { 'L', FlowerL },
+            { 'M', FlowerM },
+            { 'N', RockN },
+            { 'O', RockO },
+            { 'P', RockP },
+            { 'Q', RockQ },
+            { 'R', WallR },
+            { 'S', WallS },
+            { 'T', WallT },
+            { 'U', WallU },
+            { 'W', WaterW },
+            { 'X', WaterX },
+            { 'Y', WaterY },
+            { 'Z', WaterZ },
+            { 'a', DirtA },
+            { 'e', GrassE }
+        };
+        int i=0, j;
+        level1 = new XmlDocument();
+        level1.LoadXml(Resources.Load<TextAsset>("Level1").text);
+
+        // Map's rows:
+        foreach(XmlNode xmlNode in level1.SelectNodes("//Level/Map/Row"))
+        {
+            j = 0;
+            foreach(char currentCell in xmlNode.InnerText)
+            {
+                newCell = Instantiate(prefabs[currentCell], new Vector3(j, i), Quaternion.identity);
+                j++;
+            }
+            i--;
+        }
+
+        // Special cells:
+        foreach (XmlNode xmlNode in level1.SelectNodes("//Level/SpecialCells/Cell"))
+        {
+            switch(xmlNode.Attributes["name"].InnerText)
+            {
+                case "PiraGuchi":
+                    newCell = Instantiate(PiraGuchi, new Vector3(Convert.ToInt32(xmlNode.Attributes["posX"].Value), -Convert.ToInt32(xmlNode.Attributes["posY"].Value), -1), Quaternion.identity);
+                    break;
+                case "Enemy3F":
+                    newCell = Instantiate(Enemy3F, new Vector3(Convert.ToInt32(xmlNode.Attributes["posX"].Value), -Convert.ToInt32(xmlNode.Attributes["posY"].Value), -1), Quaternion.identity);
+                    break;
+                case "Enemy3M":
+                    newCell = Instantiate(Enemy3M, new Vector3(Convert.ToInt32(xmlNode.Attributes["posX"].Value), -Convert.ToInt32(xmlNode.Attributes["posY"].Value), -1), Quaternion.identity);
+                    break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Pirata RPG/Assets/Scripts/MovementController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Vector3 maxWalkSpeed = new Vector3(2, 2), maxRunSpeed = new Vector3(4, 4), currentMovementSpeed;
+    Vector3 deltaPos, enemyOrientation;
+    bool isRunning, isAttacking;
+    Animator currentAnimator;
+    SpriteRenderer currentSpriteRenderer;
+    bool isBlockedLeft, isBlockedRight, isBlockedTop, isBlockedBottom;
+
+    private void Awake()
+    {
+        currentAnimator = GetComponent<Animator>();
+        currentSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.tag == "Player")
+        {
+            Camera.main.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10);
+            Camera.main.transform.SetParent(gameObject.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.tag == "Player")
+        {
+            isRunning = Input.GetButton("Fire3");
+            isAttacking = Input.GetButton("Fire1");
+
+            currentMovementSpeed = new Vector3(Input.GetAxis("Horizontal") * (isRunning ? maxRunSpeed.x : maxWalkSpeed.x), Input.GetAxis("Vertical") * (isRunning ? maxRunSpeed.x : maxWalkSpeed.y));
+        }
+        else if (gameObject.tag == "Enemy" && Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, gameObject.transform.position) < 5 && Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, gameObject.transform.position) > 2)
+        {
+            isRunning = false;
+            isAttacking = false;
+
+            enemyOrientation = Vector3.zero;
+
+            if (GameObject.FindGameObjectWithTag("Player").transform.position.x > gameObject.transform.position.x)
+                enemyOrientation.x = 0.7f;
+            else
+                enemyOrientation.x = -0.7f;
+
+            if (GameObject.FindGameObjectWithTag("Player").transform.position.y > gameObject.transform.position.y)
+                enemyOrientation.y = 0.7f;
+            else
+                enemyOrientation.y = -0.7f;
+
+            currentMovementSpeed = new Vector3(enemyOrientation.x * (isRunning ? maxRunSpeed.x : maxWalkSpeed.x), enemyOrientation.y * (isRunning ? maxRunSpeed.x : maxWalkSpeed.y));
+        }
+        else if (gameObject.tag == "Enemy" && Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, gameObject.transform.position) <= 2)
+        {
+            // ¡Attack!
+            currentMovementSpeed = Vector3.zero;
+            isAttacking = true;
+        }
+        else
+        {
+            currentMovementSpeed = Vector3.zero;
+            isRunning = false;
+        }
+
+        currentAnimator.SetBool("isAttacking", isAttacking);
+
+        deltaPos = currentMovementSpeed * Time.deltaTime;
+
+        currentAnimator.SetFloat("moveSpeed", currentMovementSpeed.magnitude);
+
+        gameObject.GetComponent<Rigidbody>().velocity = currentMovementSpeed;
+        currentSpriteRenderer.flipX = currentMovementSpeed.x < 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Tree" && other.gameObject.tag != "Rock" && other.gameObject.tag != "Wall")
+            return;
+
+        if (other.gameObject.transform.position.x > gameObject.transform.position.x)
+            isBlockedRight = false;
+        if (other.gameObject.transform.position.y > gameObject.transform.position.y)
+            isBlockedTop = false;
+        if (other.gameObject.transform.position.x < gameObject.transform.position.x)
+            isBlockedLeft = false;
+        if (other.gameObject.transform.position.y < gameObject.transform.position.y)
+            isBlockedBottom = false;
+    }
+}
